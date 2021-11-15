ceacc01dc89331f5135f793e97084a8d370d7bb1
Georgi Sabev <georgethebeatle@gmail.com>
2021-11-15 09:39:49 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup the project."""
+from setuptools import setup, find_packages
+
+
+setup(
+    name='scrapecorona',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['scrapy>=2.5.0', 'dateparser>=1.1.0'],
+    url='https://github.com/georgethebeatle/scrapecorona',
+)
+
+
