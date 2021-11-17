30f524f63c798d9874af112f5a06e8ceb10dbbc5
Georgi Sabev <georgethebeatle@gmail.com>
2021-11-17 14:51:41 +0200

    git ignore and formatting

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     name='scrapecorona',
     version='0.1',
     packages=find_packages(),
-    install_requires=['scrapy>=2.5.0', 'dateparser>=1.1.0'],
+    install_requires=['scrapy>=2.5.0',
+                      'dateparser>=1.1.0',
+                      'notebook>=6.4.5',
+                      'numpy>=1.21.4',
+                      'pandas>=1.3.4',
+                      'matplotlib>=3.4.3'],
 )
-
-
